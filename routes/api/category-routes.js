272327304ed6e08cb99ff70bf5a73605527379ab
Steve Koutsodontis <skoutsodontis@gmail.com>
2021-12-15 21:37:37 -0500

    removed try and catch from category routes. errors while trying to use post and dive had enough for now

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -32,50 +32,29 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  try {
     // create a new category
-    const categoryData = await Category.
+    const requestData = await Category.
       create(req.body,);
-  } catch (err) {
-    res.status(200).json(categoryData);
-    res.status(400).json(err);
-  }
 });
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
-  try {
-    const categoryData = await Category.update(req, body, {
+      const requestData = await Category.update(req, body, {
       where: {
         id: req.params.id
       }
     })
-    if (!categoryData[0]) {
-      res.status(404).json({ message: 'No category with this id' });
-      return
-    }
-    res.status(200).json(categoryData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+    res.status(200).json(req.body);
 });
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
-  try {
     const categoryData = await Category.destroy({
       where: {
         id: req.params.id
       }
     });
-    if (!categoryData) {
-      res.status(404).json({message: 'No category found with this id'});
-      return;
-    }
     res.status(200).json(categoryData);
-    }catch (err) {
-      res.status(500).json(err);
-    }
   });
 
 module.exports = router;
